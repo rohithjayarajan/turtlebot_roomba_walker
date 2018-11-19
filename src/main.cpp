1d822e0db25f44c1419da46381de33a72ee8c17a
rohithjayarajan <rohith.jayarajan@gmail.com>
2018-11-19 00:49:29 -0500

    add template for the RoombaWalker class
    add template for the RoombaWalker class in roomba_walker.hpp and
    roomba_walker.cpp files. Add main file template

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * BSD 3-Clause License
+ *
+ * Copyright (C) 2018, Rohith Jayarajan
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *****************************************************************************/
+/**
+ *  @file    roomba_walker.hpp
+ *  @author  rohithjayarajan
+ *  @date 11/18/2018
+ *  @version 1.0
+ *
+ *  @brief main file for the turtlebot displaying a walking behavior similar to
+ * a roomba
+ *
+ *  @section DESCRIPTION
+ *
+ *  main file to implement the walking behavior similar to a roomba robot on a
+ * turtlebot
+ *
+ */
+
+#include "roomba_walker.hpp"
+
+int main(int argc, char **argv) {
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command
+   * line. For programmatic remappings you can use a different version of init()
+   * which takes remappings directly, but for most command-line programs,
+   * passing argc and argv is the easiest way to do it.  The third argument to
+   * init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "talker");
+  return 0;
+}
